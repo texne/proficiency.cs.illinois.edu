5f4878ef551997158a3dea979b53a87b21775afd
Geoffrey Challen <challen@illinois.edu>
2018-07-03 13:42:26 -0500

    Working on proficiency page.

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+	$('a').filter(function() {
+    return (this.href && this.host !== window.location.host)
+	}).each(function() {
+		$(this).attr('target', '_blank');
+	})
+})
